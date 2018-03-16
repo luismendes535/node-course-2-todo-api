94a479ad4662c5055c2aea352d4788724335f344
Luis Mendes <luismendes535@gmail.com>
2018-03-16 20:29:50 +0000

    Setup separate test and development envs

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,6 +1,7 @@
 var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://Luismendes535:root@ds115579.mlab.com:15579/todoapp'||'mongodb://localhost:27018/TodoApp');
+
+mongoose.connect(process.env.MONGODB_URI);
 
 module.exports ={mongoose};
