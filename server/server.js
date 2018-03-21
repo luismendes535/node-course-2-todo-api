c97ee4ee494239e8d2b6755c9dca350d855b3250
Luis Mendes <luismendes535@gmail.com>
2018-03-21 01:34:24 +0000

    Add POST /users/login route

--- server/server.js
+++ server/server.js
@@ -107,6 +107,32 @@ app.get('/users/me', authenticate, (req,res)=>{
     res.send(req.user);
 });
 
+app.post('/users/login',(req,res)=>{
+    var body = _.pick(req.body,['email','password']);
+
+    User.findByCredentials(body.email,body.password).then((user)=>{
+        return user.generateAuthToken().then((token)=>{
+            res.header('x-auth', token).send(user);
+        })
+    }).catch((e)=>{
+        res.status(400).send(e);
+    });
+
+    // User.findOne({email: req.body.email}).then((result)=>{
+    //     var password = req.body.password;
+        // bcrypt.compare(password, result.password,(err,res)=>{
+        //     if(!err){
+        //     res.status(200).send('SUCCESSFULLY LOGGED IN');   
+        //     }
+        //     res.send('Passwrod doesnt match');
+        // });    
+    // }).catch((e)=>{
+    //     res.send(e);
+    // });
+    // res.send(req.body);
+});
+
+
 app.listen(port,()=>{
     console.log(`Started up at port ${port}.`);
 })
